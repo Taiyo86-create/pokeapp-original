d862db571e69d343ee68bf23082651dff77c6c5d
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-02 12:28:48 +0900

    年齢を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :age_id, :sex_id, :status_id, :favorite_title_id, :profile])
+  end
 end
