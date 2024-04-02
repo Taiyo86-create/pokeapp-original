fb1a71bd2d368746c1d3808887a02bd7ba83c04a
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-02 20:42:24 +0900

    周辺機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+  end
 
+  def destroy
+    sign_out(current_user)
+    redirect_to root_path
   end
 end
