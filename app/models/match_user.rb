2bf4fa2b6da61a264c205fafc59a96757059b7af
Taiyo86-create <154989695+Taiyo86-create@users.noreply.github.com>
2024-04-07 13:45:41 +0900

    中間テーブル削除

--- app/models/match_user.rb
+++
@@ -1,4 +0,0 @@
-class MatchUser < ApplicationRecord
-  belongs_to :user
-  belongs_to :match
-end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_07_043233) do
   create_table "matches", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_matches_on_user_id"
   end
 
   create_table "rooms", charset: "utf8", force: :cascade do |t|
